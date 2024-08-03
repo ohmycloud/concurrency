1ad87dfbd76df403c16a224dff4c0565feae2e76
ohmycloudy <ohmycloudy@gmail.com>
2024-08-03 22:12:37 +0800

    small refactor

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use mpsc::metrics::Metrics;
 use rand::Rng;
 use std::thread;
@@ -6,47 +7,57 @@ use std::time::Duration;
 const N: usize = 2;
 const M: usize = 4;
 
-fn task_worker(idx: usize, mut metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics
-            .inc(format!("call.thread.worker.{}", idx))
-            .expect("inc error");
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
 
-fn request_worker(metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
 
-        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page = rng.gen_range(1..256);
-        metrics.inc(format!("req.page.{}", page));
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..256);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
-}
-fn main() {
-    let mut metrics = Metrics::new();
-    metrics.inc("req.page.1");
-    metrics.inc("call.thread.worker.1");
 
-    for i in 0..100 {
-        metrics.inc("call.thread.worker.1").expect("inc error");
+    Ok(())
+}
+fn main() -> Result<()> {
+    let metrics = Metrics::new();
+    metrics.inc("req.page.1")?;
+    metrics.inc("call.thread.worker.1")?;
+
+    for _ in 0..100 {
+        metrics.inc("call.thread.worker.1")?;
     }
 
     println!("{:?}", metrics.snapshot());
 
     // start N workers and M requesters
     for idx in 0..N {
-        task_worker(idx, metrics.clone());
+        task_worker(idx, metrics.clone())?;
     }
 
-    for idx in 0..M {
-        request_worker(metrics.clone())
+    for _ in 0..M {
+        request_worker(metrics.clone())?
     }
 
     loop {
         thread::sleep(Duration::from_millis(1000));
-        println!("{:?}", metrics.snapshot());
+        println!("{:?}", metrics.snapshot()?);
     }
 }
